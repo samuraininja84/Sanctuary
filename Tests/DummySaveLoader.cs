b3d85b453e65973cd9ce20411169014cfde8cd1f
Samuraininja84 <158503209+samuraininja84@users.noreply.github.com>
2026-06-21 01:30:29 -0400

    Created Tests Assembly

--- Tests/DummySaveLoader.cs
+++ Tests/DummySaveLoader.cs
@@ -2,42 +2,42 @@
 using Sanctuary;
 using Sanctuary.Loaders;
 
-namespace Sanctaury.Samples
+namespace Sanctaury.Tests
 {
-    ///// <summary>
-    ///// A dummy save loader that does nothing.
-    ///// </summary>
-    //public class DummySaveLoader : ISaveLoader
-    //{
-    //    private bool _exists;
+    /// <summary>
+    /// A dummy save loader that does nothing.
+    /// </summary>
+    public class DummySaveLoader : ISaveLoader
+    {
+        private bool _exists;
 
-    //    public Task<string> GetName() => Task.FromResult("Dummy Save");
+        public Task<string> GetName() => Task.FromResult("Dummy Save");
 
-    //    public Task<bool> Exists() => Task.FromResult(_exists);
+        public Task<bool> Exists() => Task.FromResult(_exists);
 
-    //    public Task<ISaveData> Load() => Task.FromResult<ISaveData>(new SaveData());
+        public Task<ISaveData> Load() => Task.FromResult<ISaveData>(new SaveData());
 
-    //    public Task Save(ISaveData data) => Task.CompletedTask;
+        public Task Save(ISaveData data) => Task.CompletedTask;
 
-    //    public Task<ISaveData> Create()
-    //    {
-    //        // Mark the save as existing.
-    //        _exists = true;
+        public Task<ISaveData> Create()
+        {
+            // Mark the save as existing.
+            _exists = true;
 
-    //        // Create a new, empty save data.
-    //        return Task.FromResult<ISaveData>(new SaveData());
-    //    }
+            // Create a new, empty save data.
+            return Task.FromResult<ISaveData>(new SaveData());
+        }
 
-    //    public Task Delete()
-    //    {
-    //        // Mark the save as not existing.
-    //        _exists = false;
+        public Task Delete()
+        {
+            // Mark the save as not existing.
+            _exists = false;
 
-    //        // Return a completed task.
-    //        return Task.CompletedTask;
-    //    }
+            // Return a completed task.
+            return Task.CompletedTask;
+        }
 
-    //    public void SetDirectory(string subdirectory) { }
-    //}
+        public void SetDirectory(string subdirectory) { }
+    }
 }
 
