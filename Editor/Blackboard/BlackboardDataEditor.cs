bf31b7d5c5cd60cbcf12afaf26b52bbf4992a462
Samuraininja84 <158503209+samuraininja84@users.noreply.github.com>
2026-06-26 17:37:59 -0400

    Update BlackboardDataEditor.cs

--- Editor/Blackboard/BlackboardDataEditor.cs
+++ Editor/Blackboard/BlackboardDataEditor.cs
@@ -16,16 +16,20 @@ namespace Sanctuary.Editor
             // Gap between fields
             float gap = 2;
 
-            entryList = new ReorderableList(serializedObject, serializedObject.FindProperty("entries"), true, true, true, true)
+            // Create a ReorderableList for the entries property
+            entryList = new ReorderableList(serializedObject, serializedObject.FindProperty(nameof(BlackboardData.entries)), true, true, true, true)
             {
+                // Define how the header of the list should be drawn
                 drawHeaderCallback = rect => 
                 {
+                    // Draw the header labels for the list
                     EditorGUI.LabelField(new Rect(rect.x, rect.y, rect.width * 0.2f, EditorGUIUtility.singleLineHeight), "Type");
                     EditorGUI.LabelField(new Rect(rect.x + rect.width * 0.2f + 10 + gap, rect.y, rect.width * 0.3f, EditorGUIUtility.singleLineHeight), "Key");
                     EditorGUI.LabelField(new Rect(rect.x + rect.width * 0.5f + 5 + gap, rect.y, (rect.width * 0.5f), EditorGUIUtility.singleLineHeight), "Value");
                 }
             };
 
+            // Define how each element in the list should be drawn
             entryList.drawElementCallback = (rect, index, isActive, isFocused) => 
             {
                 // Get the element at the current index
@@ -35,42 +39,44 @@ namespace Sanctuary.Editor
                 rect.y += 2;
 
                 // Get the properties of the element
-                var valueType = element.FindPropertyRelative("valueType");
-                var keyName = element.FindPropertyRelative("keyName");
-                var value = element.FindPropertyRelative("value");
+                var valueType = element.FindPropertyRelative(nameof(BlackboardEntryData.valueType));
+                var keyName = element.FindPropertyRelative(nameof(BlackboardEntryData.keyName));
+                var value = element.FindPropertyRelative(nameof(BlackboardEntryData.value));
 
                 // Draw the properties
                 var valueTypeRect = new Rect(rect.x, rect.y, rect.width * 0.2f, EditorGUIUtility.singleLineHeight);
                 var keyNameRect = new Rect(rect.x + rect.width * 0.2f + gap, rect.y, rect.width * 0.3f, EditorGUIUtility.singleLineHeight);
                 var valueRect = new Rect(rect.x + rect.width * 0.5f + gap * 2, rect.y, (rect.width * 0.5f) - gap, EditorGUIUtility.singleLineHeight);
 
+                // Draw the value type and key name fields
                 EditorGUI.PropertyField(valueTypeRect, valueType, GUIContent.none);
                 EditorGUI.PropertyField(keyNameRect, keyName, GUIContent.none);
 
+                // Draw the value field based on the selected value type
                 switch ((AnyValue.ValueType)valueType.enumValueIndex)
                 {
                     case AnyValue.ValueType.Int:
-                        var intValue = value.FindPropertyRelative("intValue");
+                        var intValue = value.FindPropertyRelative(nameof(AnyValue.intValue));
                         EditorGUI.PropertyField(valueRect, intValue, GUIContent.none);
                         break;
                     case AnyValue.ValueType.Float:
-                        var floatValue = value.FindPropertyRelative("floatValue");
+                        var floatValue = value.FindPropertyRelative(nameof(AnyValue.floatValue));
                         EditorGUI.PropertyField(valueRect, floatValue, GUIContent.none);
                         break;
                     case AnyValue.ValueType.Bool:
-                        var boolValue = value.FindPropertyRelative("boolValue");
+                        var boolValue = value.FindPropertyRelative(nameof(AnyValue.boolValue));
                         EditorGUI.PropertyField(valueRect, boolValue, GUIContent.none);
                         break;
                     case AnyValue.ValueType.String:
-                        var strValue = value.FindPropertyRelative("stringValue");
+                        var strValue = value.FindPropertyRelative(nameof(AnyValue.stringValue));
                         EditorGUI.PropertyField(valueRect, strValue, GUIContent.none);
                         break;
                     case AnyValue.ValueType.Vector3:
-                        var vec3Value = value.FindPropertyRelative("vector3Value");
+                        var vec3Value = value.FindPropertyRelative(nameof(AnyValue.vector3Value));
                         EditorGUI.PropertyField(valueRect, vec3Value, GUIContent.none);
                         break;
                     case AnyValue.ValueType.Object:
-                        var objectValue = value.FindPropertyRelative("objectValue");
+                        var objectValue = value.FindPropertyRelative(nameof(AnyValue.objectValue));
                         EditorGUI.PropertyField(valueRect, objectValue, GUIContent.none);
                         break;
                     default:
