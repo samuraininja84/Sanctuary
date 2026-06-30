579b495e30546b03920a081eaf2c90d9ec23c41e
Samuraininja84 <158503209+samuraininja84@users.noreply.github.com>
2026-06-30 03:23:50 -0400

    Removed Save Stages
    - I'll delegate that task to the classes implementing ISaveStore

--- Runtime/Contracts/ISaveStore.cs
+++ Runtime/Contracts/ISaveStore.cs
@@ -1,6 +1,4 @@
-﻿using System.Threading.Tasks;
-
-namespace Sanctuary.Stores
+﻿namespace Sanctuary.Stores
 {
     /// <summary>
     /// An interface with callbacks for saving and loading.
@@ -18,39 +16,15 @@ namespace Sanctuary.Stores
         virtual void OnCreate(SaveControllerBase save) { }
 
         /// <summary>
-        /// Prepares the current object for serialization or saving by performing any necessary state updates.  
-        /// </summary>
-        /// <param name="save">The save controller that manages the save operation. Cannot be null.</param>
-        virtual Task PrepareForSave(SaveControllerBase save) => Task.CompletedTask;
-
-        /// <summary>
         /// Invoked right before the data is saved to the memory.
         /// </summary>
         /// <param name="save">The save controller that provides context or state required for the load operation. Cannot be null.</param>
         void OnSave(SaveControllerBase save);
 
         /// <summary>
-        /// Performs additional processing after a save operation completes.
-        /// </summary>
-        /// <param name="save">The controller managing the save operation. Cannot be null.</param>
-        virtual Task PostSave(SaveControllerBase save) => Task.CompletedTask;
-
-        /// <summary>
-        /// Prepares the current instance for loading by performing any necessary initialization using the specified save controller.
-        /// </summary>
-        /// <param name="save">The save controller that provides context or state required for the load operation. Cannot be null.</param>
-        virtual Task PrepareForLoad(SaveControllerBase save) => Task.CompletedTask;
-
-        /// <summary>
         /// Invoked when the data is loaded from the memory.
         /// </summary>
         /// <param name="save">The save controller that provides context or state required for the load operation. Cannot be null.</param>
         void OnLoad(SaveControllerBase save);
-
-        /// <summary>
-        /// Performs additional processing after a load operation completes.
-        /// </summary>
-        /// <param name="save">The save controller that provides context or state required for the load operation. Cannot be null.</param>
-        virtual Task PostLoad(SaveControllerBase save) => Task.CompletedTask;
     }
 }
