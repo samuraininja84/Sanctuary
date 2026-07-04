819351ce50052d1177d8f975eb34b9421b427714
Samuraininja84 <158503209+samuraininja84@users.noreply.github.com>
2026-07-03 22:34:18 -0400

    Update SanctuaryServiceExtensions.cs

--- Runtime/Extensions/SanctuaryServiceExtensions.cs
+++ Runtime/Extensions/SanctuaryServiceExtensions.cs
@@ -1,9 +1,28 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace Sanctuary.Extensions
 {
     public static class SanctuaryServiceExtensions
     {
+        public static async Task<DateTime> LastModifiedTime(this ISanctuaryService service, string slotId)
+        {
+            // Get the slot information for the specified slot ID
+            var slot = service.GetSlot(slotId) ?? throw new InvalidOperationException($"Slot with ID '{slotId}' does not exist.");
+
+            // Return the last modified time of the specified slot
+            return slot.LastSaveTime;
+        }
+
+        public static async Task<double> PlayTime(this ISanctuaryService service, string slotId)
+        {
+            // Get the slot information for the specified slot ID
+            var slot = service.GetSlot(slotId) ?? throw new InvalidOperationException($"Slot with ID '{slotId}' does not exist.");
+
+            // Return the total playtime in seconds
+            return slot.TotalPlayTimeSeconds;
+        }
+
         public static async Task DeleteAllAsync(this ISanctuaryService service)
         {
             // Get all available slots from the service
