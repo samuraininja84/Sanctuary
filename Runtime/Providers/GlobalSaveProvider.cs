0163a210c274a3821de5e3904786bbe4206eeb90
Samuraininja84 <158503209+samuraininja84@users.noreply.github.com>
2026-07-04 21:09:54 -0400

    Revert "Removed FileSaveDataProvider"
    This reverts commit ff3a61e77758aff135a32e5cc671be750a6bd480.

--- Runtime/Providers/GlobalSaveProvider.cs
+++ Runtime/Providers/GlobalSaveProvider.cs
@@ -43,7 +43,7 @@ namespace Sanctuary
             // Create a new instance of the SanctuaryService with the specified configuration and components
             return SanctuaryService.Create
             (
-                new StreamSaveDataProvider(config),
+                new FileSaveDataProvider(config),
                 new JsonSaveSerializer(config),
                 new Sha256IntegrityValidator(),
                 new UnityDebugLogger(),
